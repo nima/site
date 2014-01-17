c4b152bb26b0b18c5e9c046524cde652ef591306
Nima Talebi <nima@it.net.au>
2014-01-18 00:53:48 +1100

    Updated `module/gpg': new, fix, cleanup, unit
    New:
    
        To encapsulat the gpg executable, two new functions have been
        introduced:
    
            :gpg:encrypt()
            :gpg:decrypt()
    
        And also added these new functions:
    
            :gpg:kid (key id)
            :gpg:list
    
    Update:
    
        Hide `:gpg:kid()' into a private function as it no longer needs to
        be exposed to other modules.
    
    Fix:
    
        Clean and fix of some minor issues.
    
        Unit-testing updated accordingly.

--- lib/libsh/libsite/unit/static-gpg.sh
+++ lib/libsh/libsite/unit/static-gpg.sh
@@ -1,76 +1,110 @@
 # vim: tw=0:ts=4:sw=4:et:ft=bash
+declare -A FILES=(
+    [data_orig]="/tmp/site-gpg-unit-test-data"
+    [data_encr]="/tmp/site-gpg-unit-test-data.enc"
+    [data_decr]="/tmp/site-gpg-unit-test-data.dec"
+    [key_cnf]="~/.gnupg/*.UNITTEST.*.conf"
+    [key_sec]="~/.gnupg/*.UNITTEST.*.sec"
+    [key_pub]="~/.gnupg/*.UNITTEST.*.pub"
+)
+
+function gpgSetUp() {
+    for file in ${!FILES[@]}; do
+        eval rm -f ${FILES[${file}]}
+    done
+}
+
+function gpgTearDown() {
+    for file in ${!FILES[@]}; do
+        eval rm -f ${FILES[${file}]}
+    done
+}
+
 function testCoreGpgImport() {
     core:softimport gpg
     assertEquals 0x1 0 $?
 }
 
 function test_1_1_CoreGpgKeypathPrivate() {
-    core:softimport gpg
+    core:import gpg
 
-    PROFILE=UNITTEST ::gpg:keypath '.' >${stdoutF?} 2>${stderrF?}
+    SITE_PROFILE=UNITTEST ::gpg:keypath '.' >${stdoutF?} 2>${stderrF?}
     assertTrue 0x1 $?
     assertEquals 0x2 1 $(cat ${stdoutF}|wc -w)
 
-    PROFILE=UNITTEST ::gpg:keypath '*' >${stdoutF?} 2>${stderrF?}
+    SITE_PROFILE=UNITTEST ::gpg:keypath '*' >${stdoutF?} 2>${stderrF?}
     assertFalse 0x3 $?
     assertEquals 0x4 0 $(cat ${stdoutF}|wc -w)
 }
 
 function test_2_1_CoreGpgListInternal() {
-    core:softimport gpg
+    core:import gpg
 
     #. Should be none to list at first
-    PROFILE=UNITTEST :gpg:list >${stdoutF?} 2>${stderrF?}
+    SITE_PROFILE=UNITTEST :gpg:list '*' >${stdoutF?} 2>${stderrF?}
     assertFalse 0x1 $?
 }
 
 function test_2_2_CoreGpgCreateInternal() {
-    core:softimport gpg
+    core:import gpg
 
     #. Create one
-    PROFILE=UNITTEST :gpg:create >${stdoutF?} 2>${stderrF?}
+    SITE_PROFILE=UNITTEST :gpg:create >${stdoutF?} 2>${stderrF?}
     assertTrue 0x1 $?
 
     #. List it
-    PROFILE=UNITTEST :gpg:list >${stdoutF?} 2>${stderrF?}
+    SITE_PROFILE=UNITTEST :gpg:list '*' >${stdoutF?} 2>${stderrF?}
     assertTrue 0x2 $?
 
-    local gpgkid=( $(cat ${stdoutF?}) )
+    local -a gpgkid=( $(cat ${stdoutF?}) )
     assertEquals 0x3 2 ${#gpgkid[@]}
     assertEquals 0x4 10 ${#gpgkid[1]}
 }
 
-function test_2_3_CoreGpgDeleteInternal() {
-    core:softimport gpg
+function test_2_3_CoreGpgKidPrivate() {
+    core:import gpg
+
+    local -a gpgkid_a=( $(cat ${stdoutF?}) )
+    local gpgkid_b="$(SITE_PROFILE=UNITTEST ::gpg:kid)"
+    assertEquals 0x1 "${gpgkid_a[1]}" "${gpgkid_b}"
+
+    if [ $? -eq 0 ]; then
+        local gpgkid_c=$(SITE_PROFILE=UNITTEST ::gpg:kid ${gpgkid_b})
+        assertEquals 0x2 "${gpgkid_c}" "${gpgkid_b}"
+    fi
+}
+
+function test_2_4_CoreGpgDeleteInternal() {
+    core:import gpg
 
     local gpgkid=( $(cat ${stdoutF?}) )
 
     #. Delete it
-    PROFILE=UNITTEST :gpg:delete ${gpgkid[1]} >${stdoutF?} 2>${stderrF?}
+    SITE_PROFILE=UNITTEST :gpg:delete ${gpgkid[1]} >${stdoutF?} 2>${stderrF?}
     assertTrue 0x1 $?
 
     #. Should be none to list again
-    PROFILE=UNITTEST :gpg:list >${stdoutF?} 2>${stderrF?}
+    SITE_PROFILE=UNITTEST :gpg:list '*' >${stdoutF?} 2>${stderrF?}
     assertFalse 0x2 $?
 }
 
 function test_3_1_CoreGpgListPublic() {
-    core:softimport gpg
+    core:import gpg
 
     #. Should be none to list at first
-    PROFILE=UNITTEST core:wrapper gpg list >${stdoutF?} 2>${stderrF?}
+    SITE_PROFILE=UNITTEST core:wrapper gpg list >${stdoutF?} 2>${stderrF?}
     assertFalse 0x1 $?
 }
 
 function test_3_2_CoreGpgCreatePublic() {
-    core:softimport gpg
+    core:import gpg
 
     #. Create one
-    PROFILE=UNITTEST core:wrapper gpg create >${stdoutF?} 2>${stderrF?}
+    SITE_PROFILE=UNITTEST core:wrapper gpg create >${stdoutF?} 2>${stderrF?}
     assertTrue 0x1 $?
 
     #. List it
-    PROFILE=UNITTEST :gpg:list >${stdoutF?} 2>${stderrF?}
+    SITE_PROFILE=UNITTEST :gpg:list '*' >${stdoutF?} 2>${stderrF?}
     assertTrue 0x2 $?
     local gpgkid=( $(cat ${stdoutF?}) )
     assertEquals 0x3 2 ${#gpgkid[@]}
@@ -78,20 +112,52 @@ function test_3_2_CoreGpgCreatePublic() {
 }
 
 function test_3_3_CoreGpgDeletePublic() {
-    core:softimport gpg
+    core:import gpg
 
     local gpgkid=( $(cat ${stdoutF?}) )
     if assertEquals 0x1 2 ${#gpgkid[@]}; then
         #. Delete it
-        PROFILE=UNITTEST core:wrapper gpg delete ${gpgkid[1]} >${stdoutF?} 2>${stderrF?}
+        SITE_PROFILE=UNITTEST core:wrapper gpg delete ${gpgkid[1]} >${stdoutF?} 2>${stderrF?}
         assertTrue 0x1 $?
 
         #. Try to delete it again and fail
-        PROFILE=UNITTEST core:wrapper gpg delete ${gpgkid[1]} >${stdoutF?} 2>${stderrF?}
+        SITE_PROFILE=UNITTEST core:wrapper gpg delete ${gpgkid[1]} >${stdoutF?} 2>${stderrF?}
         assertFalse 0x2 $?
 
         #. Should be none to list again
-        PROFILE=UNITTEST core:wrapper gpg list >${stdoutF?} 2>${stderrF?}
+        SITE_PROFILE=UNITTEST core:wrapper gpg list >${stdoutF?} 2>${stderrF?}
         assertFalse 0x3 $?
     fi
+}
+
+function test_4_1_CoreGpgEncryptInternal() {
+    core:import gpg
+
+    #. Create it
+    SITE_PROFILE=UNITTEST :gpg:create >${stdoutF?} 2>${stderrF?}
+
+    dd if=/dev/urandom of=${FILES[data_orig]} bs=1024 count=1024 2>/dev/null
+    SITE_PROFILE=UNITTEST :gpg:encrypt\
+        ${FILES[data_orig]} ${FILES[data_encr]} >${stdoutF?} 2>${stderrF?}
+    assertTrue 0x1 $?
+}
+
+function test_4_2_CoreGpgDecryptInternal() {
+    core:import gpg
+
+    SITE_PROFILE=UNITTEST :gpg:encrypt\
+        ${FILES[data_encr]} ${FILES[data_decr]} >${stdoutF?} 2>${stderrF?}
+    assertTrue 0x1 $?
+
+    local match=$(
+        cat ${FILES[data_orig]} ${FILES[data_decr]} |
+            md5sum | awk '{print$1}' | wc -l
+    )
+
+    assertEquals 0x2 ${match} 1
+
+    #. Delete it
+    SITE_PROFILE=UNITTEST :gpg:list '*' >${stdoutF?} 2>${stderrF?}
+    local -a gpgkid=( $(cat ${stdoutF?}) )
+    SITE_PROFILE=UNITTEST :gpg:delete ${gpgkid[1]} >${stdoutF?} 2>${stderrF?}
 }
