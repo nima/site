ffbf37deeee550d6a7b1958cf1a44c110995c9e0
Nima Talebi <nima@it.net.au>
2014-02-17 20:52:43 +1100

    Updated unit-testing/citm/travis framework
    Updated `.travis.yml': re-ordered and enabled all
    
        All primary modules have been enabled, and ordered as per
        dependency rather than anything else.  Instead of disabling them
        again for the next stable release, the incomplete modules have
        been added to the `allow_failures' list.
    
    Updated `module/unit':
    
        Added awareness for the new `dot' notation defining the output
        format of the given function (for example `.csv', or `.json') -
        there slipped under the radar and never got tested since this new
        naming standard was introduced into site.
    
        Perform set-up of the CI/UT host, which effectively means you should
        never run this on your own machine.
    
        Forced the order of check to be `private', `internal', then
        `public'; it used to be extracted from a hash and unordered.
    
        Perform the scaffolding as far as setting up the `/etc/hosts' and
        ssh user and host keys, module unit-tests that currently do this
        can (should) hence disable that code.
    
    Updated `tests/tunnel-static.sh':
    
        Scaffolding code no longer needed as it is handled by
        `module/unit'.
    
    Updated `examples/site.conf.eg':
    
        Added examples that align with the unit-testing out-of-the-box.
    
        Removed the `.' tldid, as this is now handled in implicitly, and
        make `.' the default user-defined tldid.
    
    Update `unit/citm': verbosity
    
        New function called `run' which all commands should be wrapped
        with; this way it will print out what it is going to do, so makes
        trouble-shooting from the CI output easier.

--- share/unit/tests/tunnel-static.sh
+++ share/unit/tests/tunnel-static.sh
@@ -20,34 +20,7 @@ function tunnelSetUp() {
             : noop
         ;;
         execute)
-            export g_QDN_A="host-a.unit-test.site.org"
             export g_PID=0
-
-            ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa
-            cat <<! | sudo tee -a /etc/hosts >/dev/null
-127.0.0.1   ${g_QDN_A}
-127.0.0.2   host-b.unit-test.site.org
-127.0.0.3   host-c.unit-test.site.org
-127.0.0.4   host-d.unit-test.site.org
-127.0.0.5   host-e.unit-test.site.org
-127.0.0.6   host-f.unit-test.site.org
-127.0.0.7   host-g.unit-test.site.org
-127.0.0.8   host-h.unit-test.site.org
-127.0.0.9   host-i.unit-test.site.org
-!
-            touch ~/.ssh/known_hosts
-
-            local ip
-            for ip in 127.0.0.{1..9}; do
-                ssh-keyscan -t rsa ${ip} >> ~/.ssh/known_hosts 2>/dev/null
-            done
-
-            local hostname
-            for hostname in host-{a..i}.unit-test.site.org; do
-                ssh-keyscan -t rsa ${hostname} >> ~/.ssh/known_hosts 2>/dev/null
-            done
-
-            install -m 400 ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
         ;;
         *)
             exit 127
@@ -58,21 +31,21 @@ function tunnelSetUp() {
 function test_1_0_CoreTunnelStartPublic() {
     core:import tunnel
 
-    core:wrapper tunnel start ${g_QDN_A} >${stdoutF?} 2>${stderrF?}
+    core:wrapper tunnel start host-8c.unit-tests.mgmt.site >${stdoutF?} 2>${stderrF?}
     assertTrue "0x1" $?
 }
 
 function test_1_1_CoreTunnelStartInternal() {
     core:import tunnel
 
-    :tunnel:start ${g_QDN_A}
+    :tunnel:start host-8c.unit-tests.mgmt.site
     assertEquals "0x2" ${CODE_E01?} $?
 }
 
 function test_1_2_CoreTunnelStartPublic() {
     core:import tunnel
 
-    core:wrapper tunnel start ${g_QDN_A} >${stdoutF?} 2>${stderrF?}
+    core:wrapper tunnel start host-8c.unit-tests.mgmt.site >${stdoutF?} 2>${stderrF?}
     assertTrue "0x1" $?
 }
 
@@ -82,20 +55,20 @@ function test_1_3_CoreTunnelCreateInternal() {
     :net:localportping 8000
     assertFalse "0x1" $?
 
-    :tunnel:create ${g_QDN_A} localhost 8000 localhost 22
+    :tunnel:create host-8c.unit-tests.mgmt.site localhost 8000 localhost 22
     assertTrue "0x2" $?
 
     :net:localportping 8000
     assertTrue "0x3" $?
 
-    :tunnel:create ${g_QDN_A} localhost 8000 localhost 22
+    :tunnel:create host-8c.unit-tests.mgmt.site localhost 8000 localhost 22
     assertEquals "0x4" ${CODE_E01?} $?
 }
 
 function test_1_4_CoreTunnelCreatePublic() {
     core:import tunnel
 
-    core:wrapper tunnel create ${g_QDN_A}\
+    core:wrapper tunnel create host-8c.unit-tests.mgmt.site\
         -l localhost 8000 -r localhost 22 >${stdoutF?} 2>${stderrF?}
     assertEquals "0x4" ${CODE_E01?} $?
 }
@@ -103,14 +76,14 @@ function test_1_4_CoreTunnelCreatePublic() {
 function test_1_5_CoreTunnelStatusPublic() {
     core:import tunnel
 
-    core:wrapper tunnel status ${g_QDN_A} >${stdoutF?} 2>${stderrF?}
+    core:wrapper tunnel status host-8c.unit-tests.mgmt.site >${stdoutF?} 2>${stderrF?}
     assertTrue "0x0" $?
 }
 
 function test_1_6_CoreTunnelPidInternal() {
     core:import tunnel
 
-    g_PID=$(:tunnel:pid ${g_QDN_A})
+    g_PID=$(:tunnel:pid host-8c.unit-tests.mgmt.site)
     assertTrue "0x1" $?
 
     [ ${g_PID} -gt 0 ]
@@ -132,13 +105,13 @@ function test_1_8_CoreTunnelStopInternal() {
     core:import tunnel
 
     local -i pid
-    pid=$(:tunnel:stop ${g_QDN_A})
+    pid=$(:tunnel:stop host-8c.unit-tests.mgmt.site)
     assertTrue "0x0" $?
 
     [ ${pid} -eq ${g_PID} ]
     assertTrue "0x1" $?
 
-    pid=$(:tunnel:stop ${g_QDN_A})
+    pid=$(:tunnel:stop host-8c.unit-tests.mgmt.site)
     assertFalse "0x2" $?
 
     [ ${pid} -eq 0 ]
@@ -148,6 +121,6 @@ function test_1_8_CoreTunnelStopInternal() {
 function test_1_9_CoreTunnelStopPublic() {
     core:import tunnel
 
-    core:wrapper tunnel stop ${g_QDN_A} >${stdoutF?} 2>${stderrF?}
+    core:wrapper tunnel stop host-8c.unit-tests.mgmt.site >${stdoutF?} 2>${stderrF?}
     assertTrue "0x1" $?
 }
